0b0d0dd12a5abb0c2fdcda1767ca466bb7301588
DarkFuria <robinhood010501@gmail.com>
2020-09-16 09:49:43 +0300

    Minor improvements. Last version worked, but in a majority of cases output was quite uninteresing. Minimize scope of mutex usage.

--- week5/ex3.c
+++ week5/ex3.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 8192
 
 pthread_cond_t buffer_is_empty;
 pthread_cond_t buffer_is_full;
-pthread_mutex_t count_mutex;
+pthread_mutex_t
+    count_mutex;  // used only in pthread_cond_wait. Locks right before it & unlocks right after it
 
 int buffer[BUFFER_SIZE];
 int buffer_counter = 0;
@@ -17,14 +18,18 @@ void *producer(void *args) {
     while (1) {
         // Generating new item
         int item = items_produced++;
+        usleep(20);  // kind of intensive computations, w/o it locks after first iterations
         // Checking if there is a place on stack
         if (buffer_counter == BUFFER_SIZE) {
             printf("PRODUCER goes to sleep\n");
+
+            // Sleep
             pthread_mutex_lock(&count_mutex);
             pthread_cond_wait(&buffer_is_empty, &count_mutex);
+            pthread_mutex_unlock(&count_mutex);
+
             printf("PRODUCER wakes up\n");
         }
-        pthread_mutex_lock(&count_mutex);
         // Putting number on common stack
         buffer[buffer_counter] = item;
         // Incrementing counter
@@ -33,7 +38,6 @@ void *producer(void *args) {
         if (buffer_counter == 1) {
             pthread_cond_signal(&buffer_is_full);
         }
-        pthread_mutex_unlock(&count_mutex);
     }
     pthread_exit(EXIT_SUCCESS);
     return NULL;
@@ -46,10 +50,10 @@ void *consumer(void *args) {
             printf("CONSUMER goes to sleep\n");
             pthread_mutex_lock(&count_mutex);
             pthread_cond_wait(&buffer_is_full, &count_mutex);
+            pthread_mutex_unlock(&count_mutex);
             printf("CONSUMER wakes up\n");
         }
 
-        pthread_mutex_lock(&count_mutex);
         // Taking last item
         int item = buffer[buffer_counter - 1];
         // Decrementing counter
@@ -60,14 +64,14 @@ void *consumer(void *args) {
         }
 
         // working with item... Somehow... Really intensive computations
-        usleep(60000);
-        pthread_mutex_unlock(&count_mutex);
     }
+    pthread_exit(EXIT_SUCCESS);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t producer_thread, consumer_thread;
+    pthread_mutex_init(&count_mutex, NULL);
     pthread_create(&producer_thread, NULL, &producer, NULL);
     pthread_create(&consumer_thread, NULL, &consumer, NULL);
 
